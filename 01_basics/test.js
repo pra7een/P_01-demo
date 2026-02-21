d5399f91046aa16394827f16798c93a53715a255
pra7een <praveenkrmahto61@gmail.com>
2026-02-21 18:04:59 +0530

    something

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,6 @@
-console.log("fire in the hole")
+let name = "abhay kumar lohar"
+let father = "ajay kumar lohar"
+let mother = "sobha devi"
+let brother= "chota ajay"
+let sister = "abha ig"
+console.table([name,father, mother, brother, sister]);
